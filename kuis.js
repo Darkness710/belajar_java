e6ac080ebd39d3df3ba395454158e4343fd14557
Darkness710 <133749753+Darkness710@users.noreply.github.com>
2023-10-21 01:55:48 +0000

    kuis java

---
+++ kuis.js
@@ -0,0 +1,8 @@
+let firstName, lastName, a, b;
+firstName = "Jhon";
+lastName = "doe";
+let isMarried = true;
+
+const age = 25 ;
+
+console.log(" my name = " + firstName, lastName, "\n age = " + age, "\n isMarried = " + isMarried)
